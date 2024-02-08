9b4396f958ee30cc890aad7f76efd25a7f25e1a1
Ivan Mera <imera92@gmail.com>
2024-02-08 16:33:59 -0500

    rutas para los controladores

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\DepartamentosController;
+use App\Http\Controllers\CargosController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::apiResource('usuarios', UsuariosController::class);
+Route::get('departamentos', [DepartamentosController::class, 'index']);
+Route::get('cargos', [CargosController::class, 'index']);
